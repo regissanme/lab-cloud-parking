fa995141e704ba9e8dcc2288d03d23a8a48c21c1
regissanme <rsanme@gmail.com>
2022-08-18 15:21:50 -0300

    update Swagger configs

--- src/main/java/com/rsanme/parking/config/SwaggerConfig.java
+++ src/main/java/com/rsanme/parking/config/SwaggerConfig.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 /**
  * Projeto: lab-cloud-parking
@@ -20,12 +23,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket getDocket(){
+    public Docket getDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.rsanme.parking"))
                 .build()
-                .apiInfo(metaData());
+                .apiInfo(metaData())
+                .securityContexts(List.of(actuatorSecurityContext()))
+                .securitySchemes(List.of(basicAuthScheme()));
+    }
+
+    private SecurityScheme basicAuthScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+
+    private SecurityReference basicAuthReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+    }
+
+    private SecurityContext actuatorSecurityContext() {
+        return SecurityContext.builder()
+                .securityReferences(List.of(basicAuthReference()))
+                .build();
     }
 
     private ApiInfo metaData() {
